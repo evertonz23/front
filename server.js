1f7912548a6b0d34ce4afb340b5b2ba9a4453cb3
Everton Zanatta <everton.zanatta@cwi.com.br>
2017-12-19 01:06:40 -0200

    'v2'

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use('/', express.static('public'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
